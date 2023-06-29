8c37e28b42e19b07fec3f3a2fa7bb5b4e9d03c8d
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2023-06-29 17:34:52 +0300

    Updated to Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,68 +1,42 @@
 
 import './App.css';
-import React, {Component} from 'react'; 
+import React, { useState, useEffect} from 'react'; 
 import RatesList from '../components/RateCardsList';
 import BaseCurrencySelector from '../components/BaseCurrencySelector';
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state={
-        rates: {},
-        baseCurrency:'USD',
-        displayedCurrencies:['USD','AUD','EUR','UAH','CAD','CHF','PLN','GBP','JPY','CNY']
-    }
-  }
+function App(){
+  const [rates, setRates] = useState({});
+  const [baseCurrency, setBaseCurrency] = useState('USD');
+  const [displayedCurrencies] = useState(['USD','AUD','EUR','UAH','CAD','CHF','PLN','GBP','JPY','CNY']);
 
-  componentDidMount(){
-    const url = 'https://exchangerate-api.p.rapidapi.com/rapid/latest/USD';
-    const options = {
-	      method: 'GET',
-	      headers: {
-		            'X-RapidAPI-Key': 'fb450507b7mshe0d700e7437dabbp1ca7efjsndfb748fc27f7',
-		            'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
-	            }
-        };
 
-    fetch(url, options)
-      .then(response => response.json())
-      .then(result => this.setState({rates: result['rates']}))
-	    .catch(error => console.log(error));
-  }
-  
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.baseCurrency !== prevState.baseCurrency ) {
-      const url = `https://exchangerate-api.p.rapidapi.com/rapid/latest/${this.state.baseCurrency}`;
+  useEffect(()=>{
+      const url = `https://exchangerate-api.p.rapidapi.com/rapid/latest/${baseCurrency}`;
       const options = {
-	      method: 'GET',
-	      headers: {
-		            'X-RapidAPI-Key': 'fb450507b7mshe0d700e7437dabbp1ca7efjsndfb748fc27f7',
-		            'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
-	            }
-        };
+          method: 'GET',
+          headers: {
+                  'X-RapidAPI-Key': 'fb450507b7mshe0d700e7437dabbp1ca7efjsndfb748fc27f7',
+                  'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
+                }
+          };
 
-    fetch(url, options)
-      .then(response => response.json())
-      .then(result => this.setState({rates: result['rates']}))
-      .then(console.log(this.state.rates))
-	    .catch(error => console.log(error));
-    }
-  }
-
-  onSelectChange = (selected) => {
-        this.setState({ baseCurrency: selected.value })
+      fetch(url, options)
+        .then(response => response.json())
+        .then(result => setRates(result['rates']))
+        .catch(error => console.log(error));
+  },[baseCurrency]);
+  
+  const onSelectChange = (selected) => {
+      setBaseCurrency(selected.value);
     }
 
-  render(){
-    
-    const {rates, displayedCurrencies} = this.state; 
-    if (!Object.keys(rates).length){
+  if (!Object.keys(rates).length){
       return(
         <h1>
           Loading content ...
         </h1>
       );
-    } else {
+  } else {
       const displayedRates = displayedCurrencies.map(item => rates[item]);
       return (
         <div className="App">
@@ -73,23 +47,22 @@ class App extends Component {
             <section> 
               <BaseCurrencySelector 
                 rates={rates}
-                base={this.state.baseCurrency}
-                selectChange={this.onSelectChange}
+                base={baseCurrency}
+                selectChange={onSelectChange}
                 />
             </section>
             <section>
                <RatesList
                 displayedCurrencies={displayedCurrencies} 
                 displayedRates={displayedRates}
-                baseCurrency={this.state.baseCurrency}
+                baseCurrency={baseCurrency}
               />
             </section>
           </main>
           <footer className='App-footer'>Designed by Nataliia Kulyk - 2023</footer>
         </div>
     );
-    }   
-  } 
+  }    
 }
 
 export default App;
